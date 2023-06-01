968219d2ff2081c9af1bbadc2f4902240a1c0be0
DevxPi <macro.devxpi@gmail.com>
2023-06-01 23:31:03 +0800

    update excersices day_4

--- day_4/excersices.py
+++ day_4/excersices.py
@@ -14,3 +14,21 @@ print(company.swapcase())
 
 word = company[6:]
 print(word)
+
+company.replace('Coding For All', 'python')
+
+company = "Coding For All"
+
+print(company.split(' '))
+
+social = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon" 
+print(social.split(', '))
+
+print(company[0])
+print(company[-1])
+print(company[10])
+
+words = "Python For Everyone"
+coding = "Coding For All"
+print("".join(w[0] for w in words.split(' ')))
+print("".join(w[0] for w in coding.split(' ')))
