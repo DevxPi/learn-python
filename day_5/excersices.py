7e71a8bbacf08c37a869ea3ba7b0e75e894f6765
DevxPi <macro.devxpi@gmail.com>
2023-06-03 22:34:27 +0800

    day-5 excersices

---
+++ day_5/excersices.py
@@ -0,0 +1,32 @@
+
+car = list()
+
+another_car = []
+
+print(car)
+print(another_car)
+
+brand_car = ['PROTON', 'PRODUA', 'HONDA', 'NISSAN', 'TOYOTA', 'MAZDA']
+print(brand_car)
+print(len(brand_car))
+
+print(brand_car[0]) # first item
+print(brand_car[6-1]) # last item
+
+mixed_data_types = [
+    "Devxpi",
+    25,
+    185,
+    "single",
+    "Malaysia"
+]
+
+print(mixed_data_types)
+
+it_companies = [
+    "FACEBOOK", "GOOGLE", "MICROSOFT", "APPLE", "IBM", "ORACLE", "AMAZON"
+]
+print(it_companies)
+print(len(it_companies))
+print(it_companies[0])
+print(it_companies[len(it_companies)-1])
